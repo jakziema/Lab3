ce8949b117604d98697b17b5f373900aa872d586
Dominik <dom.kraj@gmail.com>
2017-04-11 13:35:45 +0200

    Initial commit

---
+++ src/OSMParser.java
@@ -0,0 +1,9 @@
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureCollections;
+
+/**
+ * Created by Dominik on 11.04.2017.
+ */
+public class OSMParser {
+    
+}
