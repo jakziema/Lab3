8e25aafac5f8a3d96659a8687e615dc884beff0b
Dominik <dom.kraj@gmail.com>
2017-04-11 13:50:23 +0200

    1

--- src/OSMParser.java
+++ src/OSMParser.java
@@ -1,9 +1,34 @@
-import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.feature.FeatureCollections;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by Dominik on 11.04.2017.
  */
 public class OSMParser {
-    
+
+    public static void main(String[] args) {
+
+        try {
+            URL website = new URL("http://www.openstreetmap.org/api/0.6/map?bbox=18.602856,54.320462,18.607562,54.323349");
+            URLConnection connection = website.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+
+            while ((inputLine = bufferedReader.readLine()) != null)
+                response.append(inputLine);
+
+            bufferedReader.close();
+
+            System.out.println(response.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
